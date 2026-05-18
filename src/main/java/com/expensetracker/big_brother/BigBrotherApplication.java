28bac8ac0ba4112bd29e952babc8e5d121803ab8
Abdalla312 <bodydody312@gmail.com>
2026-05-18 15:24:39 +0300

    intial project commit

---
+++ src/main/java/com/expensetracker/big_brother/BigBrotherApplication.java
@@ -0,0 +1,13 @@
+package com.expensetracker.big_brother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigBrotherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigBrotherApplication.class, args);
+    }
+
+}
