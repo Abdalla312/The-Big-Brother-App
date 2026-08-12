8f30e97af70e3a0d41b7876bf847a615e49c62ca
Abdalla312 <bodydody312@gmail.com>
2026-08-12 18:41:51 +0300

    fix(migration): consolidate email verification unique constraintinto V7

--- backend/src/main/resources/db/Migration/V7__create_email_verification_tokens_table.sql
+++ backend/src/main/resources/db/Migration/V7__create_email_verification_tokens_table.sql
@@ -1,7 +1,8 @@
 CREATE TABLE email_verification_token (
     id BIGSERIAL PRIMARY KEY,
     token_hash VARCHAR(255) NOT NULL UNIQUE,
-    user_id UUID NOT NULL UNIQUE,
+    user_id UUID NOT NULL ,
     expires_at TIMESTAMP NOT NULL,
+    CONSTRAINT uq_evt_user UNIQUE (user_id),
     CONSTRAINT fk_email_verification_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
